7974634d7055863284f65f2e6083336f4760c3e5
Ubuntu <ubuntu@ip-172-31-87-252.ec2.internal>
2019-08-26 07:37:58 +0000

    First Commit

---
+++ app.py
@@ -0,0 +1,73 @@
+from flask import Flask, render_template, request
+import os
+import pymysql
+
+connection = pymysql.connect(
+    host='localhost', # IP address of the database; localhost means "the local machine"
+    user="admin@localhost",  #the mysql user
+    password="password", #the password for the user
+    database="Chinook" #the name of database we want to use
+)
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    cursor = connection.cursor(pymysql.cursors.DictCursor)
+    sql = "SELECT * FROM Employee"
+    cursor.execute(sql)
+    # store results in a list
+    results = []
+    for r in cursor:
+        results.append(r)
+   
+    return render_template('index.html', data=results)
+
+@app.route('/artist')
+def artists():
+     cursor = connection.cursor(pymysql.cursors.DictCursor)
+     sql = "SELECT * FROM Artist"
+     cursor.execute(sql)
+     results = []
+     for r in cursor:
+         results.append(r)
+     return render_template('artist.html', data=results)
+     
+@app.route('/album/<artistId>')
+def albums(artistId):
+     cursor = connection.cursor(pymysql.cursors.DictCursor)
+     
+     sql = "SELECT * FROM Artist Where ArtistId = {}".format(artistId)
+     cursor.execute(sql)
+     artist = cursor.fetchone()
+     
+     sql = "SELECT * FROM Album WHERE ArtistId = {}".format(artistId)
+     cursor.execute(sql)
+     results = []
+     for r in cursor:
+         results.append(r)
+     return render_template('album.html', data=results, artist=artist)
+
+# @app.route('/track')
+
+# def track(albumId):
+#      cursor = connection.cursor(pymysql.cursors.DictCursor)
+     
+#      sql = "SELECT * FROM Track WHERE AlbumId = {}".format(albumId)
+#      cursor.execute(sql)
+#      results = []
+#      for r in cursor:
+#          results.append(r)
+#      return render_template('track.html', data=results)
+
+@app.route('/mediatype')
+def media():
+    cursor = connection.cursor(pymysql.cursors.DictCursor)
+    sql = "SELECT * FROM MediaType"
+    
+# "magic code" -- boilerplate
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
